14bfd4cfae867d2dffa1ceb493597cf225ca6a94
tommy-pellerin <tommytp@hotmail.fr>
2024-04-30 18:38:13 +0200

    link animation ok

--- src/PageDetail.js
+++ src/PageDetail.js
@@ -36,20 +36,20 @@ const PageDetail = (argument) => {
 
       articleDOM.querySelector("p.release-date span").innerHTML = released;
 
-      const platformNames = platforms.map(platform => platform.platform.name);
-      articleDOM.querySelector("p.platforms span").innerHTML = platformNames.join(", ");
+      const platformLinks = platforms.map(platform => `<a href="#${platform.platform.name}">${platform.platform.name}</a>`);
+      articleDOM.querySelector("div.platforms").innerHTML += platformLinks.join(", ");
       
-      const developersNames = developers.map((developer) => developer.name);
-      articleDOM.querySelector("p.developer span").innerHTML = developersNames.join(", ");
+      const developersLinks = developers.map((developer) => `<a href="#${developer.name}">${developer.name}</a>`);
+      articleDOM.querySelector("div.developer").innerHTML += developersLinks.join(", ");
       
-      const genreNames = genres.map((genre) => genre.name);
-      articleDOM.querySelector("p.genre span").innerHTML = genreNames.join(", ");
+      const genreLinks = genres.map((genre) => `<a href="#${genre.name}">${genre.name}</a>`);
+      articleDOM.querySelector("div.genre").innerHTML += genreLinks.join(", ");
 
-      const tagNames = tags.map((tag) => tag.name);
-      articleDOM.querySelector("p.tags span").innerHTML = tagNames.join(", ");
+      const tagLinks = tags.map((tag) => `<a href="#${tag.name}">${tag.name}</a>`);
+      articleDOM.querySelector("div.tags").innerHTML += tagLinks.join(", ");
 
-      const publisherNames = publishers.map((publisher) => publisher.name);
-      articleDOM.querySelector("p.publisher span").innerHTML = publisherNames.join(", ");
+      const publisherLinks = publishers.map((publisher) => `<a href="#${publisher.name}">${publisher.name}</a>`);
+      articleDOM.querySelector("div.publisher span").innerHTML += publisherLinks.join(", ");
 
       const storesParagraph = articleDOM.querySelector("div.storeBody");
       storesParagraph.innerHTML = ''; // Clear the paragraph
@@ -96,13 +96,23 @@ const PageDetail = (argument) => {
           <p class="description"><strong>Plot</strong> </br></p>
           <div class="dateDevPlatformPublisher">
             <p class="release-date"><strong>Release date</strong> </br><span></span></p>
-            <p class="developer"><strong>Developer</strong> </br><span></span></p>
-            <p class="platforms"><strong>Platforms</strong> </br><span></span></p>
-            <p class="publisher"><strong>Publisher</strong> </br><span></span></p>
+            <div class="developer">
+              <p>Developer</p>
+            </div>
+            <div class="platforms">
+            <p>Platforms</p>
+            </div>
+            <div class="publisher">
+            <p>Publisher</p>
+            </div>
           </div>
           <div class="genreTags">
-            <p class="genre"><strong>Genre</strong> </br><span></span></p>
-            <p class="tags"><strong>Tags</strong> </br><span></span></p>
+            <div class="genre">
+            <p>Genre</p>
+            </div>
+            <div class="tags">
+            <p>Tags</p>
+            </div>
           </div>
           
 
