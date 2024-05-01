20b6bb023c4a6f62533ca3fb75a11db71f664ee3
tommy-pellerin <tommytp@hotmail.fr>
2024-05-01 10:45:10 +0200

    show more button is working well

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,36 @@
 const Home = (argument = '') => {
   console.log('Home', argument);
-  const showMoreButton = document.getElementById('showMore');
+  
+  //initiate varialbe when changing template
+  window.addEventListener('unload', function() {
+    currentPage = 1;
+    storedData = null;
+  });
 
+  //preparing page to show
   const preparePage = () => {
     const cleanedArgument = argument.trim().replace(/\s+/g, '-');
     const LoadingContent = document.getElementById("loading")
     LoadingContent.style.display = "none"
 
-    // Function to fetch more results
+    // Function to fetch more results when click on "show more"
     function fetchMoreResults() {
-      console.log("click");
+      
+      console.log("click home");
       fetchList(`https://api.rawg.io/api/games?key=${process.env.API_KEY}`, cleanedArgument, currentPage);
     }
 
+    //display the fetching result in the DOM
     const displayResults = (articles, nextPageURL) => {
+      
       console.log(articles);
+      //iterate all article
       const resultsContent = articles.map((article) => {
+        //recuperate parent platforms slug of each articles
         const platformSlugs = article.parent_platforms.map((platform) => `<div class="platform${platform.platform.slug}"></div>`).join(' ');
+        //recuperate genres of each articles
         const genres = article.genres.map((genre) => genre.slug).join(", ");
+        //recuperate image of each articles
         const image = article.background_image ? `<img src="${article.background_image}" alt="poster" />` : `<div class="no_image" alt="poster" /></div>`;
         return `
           <article class="cardGame">
@@ -52,25 +65,31 @@ const Home = (argument = '') => {
           </article>`;
       });
       const resultsContainer = document.querySelector('.page-list .articles');
+      //append content to the DOM
       resultsContainer.innerHTML += resultsContent.join("\n"); // with += we Append new results to existing results
       
+      //adding show more button to the DOM
+      const showMoreBox = document.getElementsByClassName('showMoreBox')[0];
+      showMoreBox.innerHTML = `<button id="showMore">Show more</button>`;
+      const showMoreButton = document.getElementById('showMore');
       //vérifier si le nombre de card affiché est inférieur à 27
       if (nextPageURL && currentPage < 3) {
-        // If there is a next page and the current page is less than 3, display the "Show more" button
-      
+        // If there is a next page and the current page is less than 3, display the "Show more" button      
         showMoreButton.style.display = "block";
       
-        // Remove any existing event listeners
-        showMoreButton.removeEventListener('click', fetchMoreResults);
-      
-        // Add event listener
+        // Add event listener and call the functon above
         showMoreButton.addEventListener('click', fetchMoreResults);
-      }
-    };
+      };
+
+    };//end of display function
+
     let storedData = null; // This variable will store the fetched data
     let currentPage = 1; // Keep track of the current page
+
+    //fonction to fetch data with API
     const fetchList = (url, argument, page) => {
       console.log(`la page actuelle: ${page}`);
+      console.log(`argument actuel ${argument}`);
       let date = new Date();
       let currentYear = date.getFullYear();
       let month = (date.getMonth() + 1).toString().padStart(2, '0');
@@ -79,31 +98,35 @@ const Home = (argument = '') => {
       let limitYear = currentYear + 1;
       
       const finalURL = argument ? `${url}&search=${argument}&page=${page}&page_size=9&ordering=-released` : `${url}&dates=${today},${limitYear}-12-31&page=${page}&page_size=9`;
+      console.log(`final URL: ${finalURL}`);
       fetch(finalURL)
         .then((response) => response.json())
         .then((responseData) => {
           storedData = responseData; // Store the fetched data
+              
           displayResults(storedData.results,storedData.next)
           // Increment currentPage after results have been displayed
           currentPage++;
 
           // Check to hide the "Show more" button after new results have been fetched
           if (!responseData.next || currentPage > 3) {
-            if (showMoreButton) {
+            
               console.log("3 page affiché");
-              showMoreButton.style.display = "none";
               currentPage = 1
-            }
-          }
+              console.log(`page actuelle apres avoir affiché 3 page: ${currentPage}`);
+              storedData = null
+          };
+
         })
         .catch((error) => {
           console.error('Error:', error);
         });
-    };
+    };//end of fetching function
 
     fetchList(`https://api.rawg.io/api/games?key=${process.env.API_KEY}`, cleanedArgument, currentPage);
-  };
+  };//end of prepare page function
 
+  //fonction that is rendered at the begining, then it will call the rest
   const render = () => {
     welcome.innerHTML = `
     <h2>Welcome,</h2>
--- src/PageList.js
+++ src/PageList.js
@@ -1,6 +1,11 @@
 const PageList = (argument = '') => {
   console.log('PageList', argument);
   const showMoreButton = document.getElementById('showMore');
+  //initiate varialbe when changing template
+  window.addEventListener('unload', function() {
+    currentPage = 1;
+    storedData = null;
+  });
 
   const preparePage = () => {
     const cleanedArgument = argument.trim().replace(/\s+/g, '-');
@@ -9,12 +14,14 @@ const PageList = (argument = '') => {
 
     // Function to fetch more results
     function fetchMoreResults() {
-      console.log("click");
+      
+      console.log("click pagelist");
       fetchList(`https://api.rawg.io/api/games?key=${process.env.API_KEY}`, cleanedArgument, currentPage);
     };
 
     const displayResults = (articles, nextPageURL) => {
-      welcome.innerHTML = '';//delete welcom content
+      welcome.innerHTML = '';//delete welcome content
+      console.log(`articles à afficher:`);
       console.log(articles);
       const resultsContent = articles.map((article) => {
         const platformSlugs = article.parent_platforms.map((platform) => `<div class="platform${platform.platform.slug}"></div>`).join(' ');
@@ -51,14 +58,16 @@ const PageList = (argument = '') => {
       const resultsContainer = document.querySelector('.page-list .articles');
       resultsContainer.innerHTML += resultsContent.join("\n"); // with += we Append new results to existing results
       
+      //adding show more button to the DOM
+      const showMoreBox = document.getElementsByClassName('showMoreBox')[0];
+      showMoreBox.innerHTML = `<button id="showMore">Show more</button>`;
+      const showMoreButton = document.getElementById('showMore');
+
       //vérifier si le nombre de card affiché est inférieur à 27
       if (nextPageURL && currentPage < 3) {
         // If there is a next page and the current page is less than 3, display the "Show more" button
       
         showMoreButton.style.display = "block";
-      
-        // Remove any existing event listeners
-        showMoreButton.removeEventListener('click', fetchMoreResults);
       
         // Add event listener
         showMoreButton.addEventListener('click', fetchMoreResults);
@@ -69,6 +78,7 @@ const PageList = (argument = '') => {
     const fetchList = (url, argument, page) => {
       
       console.log(`la page actuelle: ${page}`);
+      console.log(`argument actuel : ${argument}`);
 
       let date = new Date();
       let currentYear = date.getFullYear();
@@ -78,21 +88,25 @@ const PageList = (argument = '') => {
       let limitYear = currentYear + 1;
       
       const finalURL = argument ? `${url}&${argument}&page=${page}&page_size=9&ordering=-released` : `${url}&dates=${today},${limitYear}-12-31&page=${page}&page_size=9`;
+      console.log(`final URL: ${finalURL}`);
       fetch(finalURL)
         .then((response) => response.json())
         .then((responseData) => {
           storedData = responseData; // Store the fetched data
+          console.log(`store data ${storedData}`); 
           displayResults(storedData.results,storedData.next)
           // Increment currentPage after results have been displayed
           currentPage++;
 
           // Check to hide the "Show more" button after new results have been fetched
           if (!responseData.next || currentPage > 3) {
-            if (showMoreButton) {
+
               console.log("3 page affiché");
-              showMoreButton.style.display = "none";
               currentPage = 1
-            }
+              console.log(`page actuelle apres avoir affiché 3 page: ${currentPage}`);
+              storedData = null
+              console.log(storedData);
+          
           }
         })
         .catch((error) => {
